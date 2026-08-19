8f41cb26ca249f57085d7d5ebdbd430cc776606f
Maryse Mongeau <vivgxojo@gmail.com>
2026-08-19 11:28:29 -0400

    Add files via upload

---
+++ presentations.py
@@ -0,0 +1,23 @@
+# --- À compléter ---
+nom = "Ton prénom"
+ecole_secondaire = "Ton école secondaire"
+programme = "Programmation ou Réseau ?"
+attentes = "Tes attentes..."
+besoins = "..."
+motivations = ""
+defis =
+forces =
+loisirs =
+
+# -------------------
+
+print("===== FICHE APPRENANT =====")
+print("Bonjour, je m’appelle :", nom)
+print("Je suis diplômé de :", ecole_secondaire)
+print("Je suis inscrit en : ", programme)
+print("Mes attentes sont :", attentes)
+print("De quoi j’ai besoin  :", besoins)
+print("Ce qui me motive :", motivations)
+print("Mes défis :", defis)
+print("Mes forces :", forces)
+print("Entre mes cours, j'aime :", loisirs)
